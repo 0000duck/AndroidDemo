aded992a4b5dadd87a8526f1c9fba48dc6b2f06d
qwinner <xiaoqiangaaa2007@163.com>
2017-06-20 16:06:14 +0800

    增加地图定位。

--- gy/app/src/main/java/wyq/gy/MainActivity.java
+++ gy/app/src/main/java/wyq/gy/MainActivity.java
@@ -1,5 +1,7 @@
 package wyq.gy;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,32 +15,59 @@ import android.view.View;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.NavigationView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdate;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.CameraPosition;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.MyLocationStyle;
+
+import wyq.gy.lib.LocationTask;
+import wyq.gy.lib.OnLocationGetListener;
+import wyq.gy.lib.PositionEntity;
+import wyq.gy.lib.RegeocodeTask;
+import wyq.gy.lib.RouteTask;
+import wyq.gy.lib.Utils;
 
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        AMap.OnCameraChangeListener,
+        AMap.OnMapLoadedListener, OnLocationGetListener, View.OnClickListener {
+
+    MapView mMapView = null;
+    private AMap mAmap;
+    private Marker mPositionMark;
+    private LatLng mStartPosition;
+    private RegeocodeTask mRegeocodeTask;
+    private LocationTask mLocationTask;
+    private ImageView mLocationImage;
+    private boolean mIsFirst = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init(savedInstanceState);
+        mLocationTask = LocationTask.getInstance(getApplicationContext());
+        mLocationTask.setOnLocationGetListener(this);
+        mLocationTask.startSingleLocate();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 //        // 测试 SDK 是否正常工作的代码
@@ -56,6 +85,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+    private void init(Bundle savedInstanceState) {
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mAmap = mMapView.getMap();
+        mAmap.getUiSettings().setZoomControlsEnabled(true);
+        mAmap.setOnMapLoadedListener(this);
+        mAmap.setOnCameraChangeListener(this);
+        mLocationImage = (ImageView) findViewById(R.id.location_image);
+        mLocationImage.setOnClickListener(this);
+
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -68,23 +109,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -112,5 +146,94 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    //地图
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onMapLoaded() {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.anchor(0.5f, 0.5f);
+        markerOptions.position(new LatLng(0, 0));
+        markerOptions
+                .icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory
+                        .decodeResource(getResources(),
+                                R.drawable.icon_loaction_start)));
+        mPositionMark = mAmap.addMarker(markerOptions);
+
+        mPositionMark.setPositionByPixels(mMapView.getWidth() / 2,
+                mMapView.getHeight() / 2);
+        mLocationTask.startSingleLocate();
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition arg0) {
+
+    }
+
+    @Override
+    public void onCameraChangeFinish(CameraPosition cameraPosition) {
+        mStartPosition = cameraPosition.target;
+        mRegeocodeTask.setOnLocationGetListener(this);
+        mRegeocodeTask
+                .search(mStartPosition.latitude, mStartPosition.longitude);
+        if (mIsFirst) {
+            Utils.addEmulateData(mAmap, mStartPosition);
+            if (mPositionMark != null) {
+                //mPositionMark.setToTop();
+            }
+            mIsFirst = false;
+        }
+    }
+
+    @Override
+    public void onLocationGet(PositionEntity entity) {
+        // todo 这里在网络定位时可以减少一个逆地理编码
+        RouteTask.getInstance(getApplicationContext()).setStartPoint(entity);
+
+        mStartPosition = new LatLng(entity.latitue, entity.longitude);
+        CameraUpdate cameraUpate = CameraUpdateFactory.newLatLngZoom(
+                mStartPosition, mAmap.getCameraPosition().zoom);
+        mAmap.animateCamera(cameraUpate);
+
+    }
+
+    @Override
+    public void onRegecodeGet(PositionEntity entity) {
+        entity.latitue = mStartPosition.latitude;
+        entity.longitude = mStartPosition.longitude;
+        RouteTask.getInstance(getApplicationContext()).setStartPoint(entity);
+        RouteTask.getInstance(getApplicationContext()).search();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.location_image:
+
+                mLocationTask.startSingleLocate();
+                break;
+        }
+    }
+
+
 
 }
